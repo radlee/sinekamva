44328f00c0f10cb0253a269d90fc0d9ea4eda0d6
radlee <prof.lolito@gmail.com>
2020-07-28 15:01:19 +0200

    Updates

---
+++ precache-manifest.0032582d508f9c4926a073f73be98cd5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "862381acba5ea1fa775cb784f664183b",
+    "url": "/sinekamva/index.html"
+  },
+  {
+    "revision": "746ffea835bf81f6ba06",
+    "url": "/sinekamva/static/css/2.bf5b17f7.chunk.css"
+  },
+  {
+    "revision": "9f2f4ea703d2e7e667eb",
+    "url": "/sinekamva/static/css/main.e18eefa0.chunk.css"
+  },
+  {
+    "revision": "746ffea835bf81f6ba06",
+    "url": "/sinekamva/static/js/2.bab41358.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/sinekamva/static/js/2.bab41358.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f2f4ea703d2e7e667eb",
+    "url": "/sinekamva/static/js/main.54abedf3.chunk.js"
+  },
+  {
+    "revision": "c5070b54ff4e498e24e6",
+    "url": "/sinekamva/static/js/runtime-main.1fa0bcc0.js"
+  },
+  {
+    "revision": "bd816289944fd312fa8ff81709b3ed16",
+    "url": "/sinekamva/static/media/bg2.bd816289.jpg"
+  },
+  {
+    "revision": "9cbeb5a1273b9b21f276e63a08249c45",
+    "url": "/sinekamva/static/media/circles.9cbeb5a1.png"
+  }
+]);
